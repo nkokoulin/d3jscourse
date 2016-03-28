e3a901fd0b7a298188d5c169f4f39eb8efa3c279
Николай Кокоулин < >
2016-03-28 15:28:10 +0300

    добавлена зависимость d3

--- assets/js/modules/d3.js
+++ assets/js/modules/d3.js
@@ -1,1 +1,29 @@
-angular.module('d3', []);;
+angular.module('d3', []).factory('d3Factory',
+  ['$document', '$q', '$rootScope', '$window', function($document, $q, $rootScope, $window) {
+
+    function onScriptLoad() {
+      // window.d3
+      $rootScope.$apply(function() {d.resolve($window.d3)});
+    }
+
+    var d = $q.defer();
+
+    var scriptTag = $document[0].createElement('script');
+    scriptTag.type = 'text/javascript';
+    scriptTag.async = true;
+    scriptTag.src = '../../vendor/d3/d3.js';
+    scriptTag.onload = onScriptLoad;
+
+    scriptTag.onreadystatechange = function() {
+      if (this.readyState = 'complete') onScriptLoad();
+    }
+
+    var b = $document[0].getElementByTagName('body')[0];
+    b.appendChild(scriptTag);
+
+    return {
+      d3: function() {return d.promise;}
+    }
+
+  }]
+);
