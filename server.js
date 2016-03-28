c80344dea23086e6589a9a19c812bee4a56a92df
Николай Кокоулин < >
2016-03-28 10:27:38 +0300

    server.js and main.jade are created

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ var express = require('express');
 var app     = express();
 var router  = express.Router();
 var path    = require('path');
+
+app.set('view engine', 'jade');
+
+app.get('/kit', function(req, res, next) {
+  res.render('main', {
+    pageTitle: 'Главная страница'
+  });
+});
+
+app.use('/', router);
+
+app.listen(8880);
